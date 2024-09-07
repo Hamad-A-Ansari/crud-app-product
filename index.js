fe2a81cb1bb11b28f9c8239b8f45071f89228809
Hamad-A-Ansari <hamad.ansarif90@gmail.com>
2024-09-07 11:51:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const mongoose = require('mongoose');//Importing Express
+const Product = require('./models/product.model.js');
+const productRoute = require("./routes/product.route.js");
+const app = express();
+
+
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false }));
+
+//routes
+app.use("/api/products", productRoute);
+
+
+app.get('/', (req, res) =>{
+  res.send(`Hello from Node API Server`);
+});
+
+
+
+
+/** 
+ * req -> request method
+ * res -> response method
+ * pBA46oJIoAjN8wCk
+*/
+
+
+
+mongoose.connect("mongodb+srv://appAdmin:pBA46oJIoAjN8wCk@crudappbackend.jjn9boo.mongodb.net/Node-API?retryWrites=true&w=majority&appName=CrudAppBackend")
+.then(()=>{
+  console.log(`Connected to DB`);
+  app.listen(3000, () =>{
+    console.log(`Server is running on port 3000`);
+  });
+})
+.catch(()=>{
+  console.log(`connection failed`);
+})
+
+/**
+ * set port to 3000 and you can access the node website and access the api
+ */
